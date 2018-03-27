c29284bae4de6c2e3dc8e6996edc4c37a90c30f6
Yuting Tao <YTao@esri.com>
2018-03-27 00:23:46 -0700

    Initial check-in
    Added first project

---
+++ Projects/FirstOpenGL/FirstOpenGL/MainProgram.cpp
@@ -0,0 +1,38 @@
+﻿#include <glad.h>
+#include <glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	//调用glfwInit函数来初始化GLFW
+	glfwInit();		
+	//使用glfwWindowHint函数来配置GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//GLAD是用来管理OpenGL的函数指针的，所以在调用任何OpenGL的函数之前我们需要初始化GLAD。
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//glViewport函数前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）。
+	//处理过的OpenGL坐标范围只为-1到1，因此我们事实上将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)。
+	glViewport(0, 0, 800, 600);
+
+	return 0;
+}
